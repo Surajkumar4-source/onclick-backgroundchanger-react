eca2e080d198eeb8954d610d22d17cd975010680
Suraj Kumar Choudhary <122175764+Surajkumar4-source@users.noreply.github.com>
2023-10-12 16:44:53 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-7 shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <div className="flex flex-wrap justify-center gap-9 shadow-lg bg-white px-3 py-5 rounded-3xl">
 
           <button 
           onClick={() => setcolor("red")} className="outline-none px-4 py-1 rounded-full bg-white shadow-lg"
